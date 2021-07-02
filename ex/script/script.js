da97e12daf0cdd1cf21cc1215e83ac257d57b5ae
dbrembilla <72857468+dbrembilla@users.noreply.github.com>
2021-07-02 13:12:45 +0200

    Delete ex directory

--- ex/script/script.js
+++
@@ -1,6 +0,0 @@
-function Select(){
-	var style = $('input[name=Style]:checked', 'Selection').val()
-	$("#SelectedStyle").attr("href", style);
-	console.log("Print")
-} ;
-$(".radio").click(Select())	;
