4e5738db2b4868d14e8ba8031d8a2b202b1e1855
ChiaraCati <72877990+ChiaraCati@users.noreply.github.com>
2021-08-16 19:07:31 +0200

    Update metadata_spg.js

--- styleplayground/metadata_spg.js
+++ styleplayground/metadata_spg.js
@@ -12,9 +12,11 @@ String.prototype.sub = function(o) { //funzione che serve a inserire gli element
 
         function main() { //recupera gli html
             getArticles("list_workfromhome.json", "#topic1"||"#topic1s", "topic1") 
-                   getArticles("list_workfromhome.json","#topic1s", "topic1")
-            getArticles("list_space.json", "#topic2", "topic2") 
-            getArticles("list_inclusivelanguage.json", "#topic3", "topic3") 
+            getArticles("list_workfromhome.json","#topic1s", "topic1")
+            getArticles("list_space.json", "#topic2", "topic2")
+            getArticles("list_space.json", "#topic2s", "topic2")
+            getArticles("list_inclusivelanguage.json", "#topic3", "topic3")
+            getArticles("list_space.json", "#topic3s", "topic3")
 
         }
         function getArticles(url, ref, mytopic){
