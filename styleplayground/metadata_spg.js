212ef0a10283996cf3e7e8a7c18f2cbe755667f1
dbrembilla <davide.brembilla98@gmail.com>
2021-09-04 14:51:27 +0200

    Update metadata_spg.js

--- styleplayground/metadata_spg.js
+++ styleplayground/metadata_spg.js
@@ -207,7 +207,7 @@ function fillInfo(from, where) { //ritornare a solo 1 where
                           
                 ` ; //meta con le keyword
             console.log(from + ' h1');
-            var titlefill = $(from + ' h1').innerText //sceglie elementi h1 nell'elemento indicato
+            var titlefill = $(from + ' h1')[0].innerText //sceglie elementi h1 nell'elemento indicato
             var authorfill = $(from + ' .auth')[0].innerText //sceglie elemento con byline con autore
             var pubfill = $(from + ' .pub')[0].innerText
 
