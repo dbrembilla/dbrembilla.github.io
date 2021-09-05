fe44c9c98466f3b57a2e0b8eb44b721ff0c92086
dbrembilla <davide.brembilla98@gmail.com>
2021-09-05 17:34:23 +0200

    Update metadata_spg.js

--- styleplayground/metadata_spg.js
+++ styleplayground/metadata_spg.js
@@ -145,7 +145,7 @@ String.prototype.sub = function(o) { //funzione che serve a inserire gli element
 
         function filltab(what,where) { //questo riempie le tabelle del metadata viewer
             var linkClass = what.replace(/\s/g, "-");
-            linkClass.replaceAll(".", "");
+            linkClass = linkClass.replaceAll(".", "");
             console.log(linkClass);
             var listFirst = `<li class="$classtodelete">$content[$links]</li>`; //ciascun elemento ha una sua riga, rimanda all'oggetto con href e ha content come l'argomento è chiamato
             var listContent = '<a id="$thisclass" href="$place">$number</a> '; //dal secondo elemento si pone a fianco di quello presente
