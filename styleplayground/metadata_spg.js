bba5ab1091253c437df001717df23efe5abf1edd
ChiaraCati <72877990+ChiaraCati@users.noreply.github.com>
2021-09-06 15:59:37 +0200

    Update metadata_spg.js

--- styleplayground/metadata_spg.js
+++ styleplayground/metadata_spg.js
@@ -149,7 +149,7 @@ String.prototype.sub = function(o) { //funzione che serve a inserire gli element
             linkClass = linkClass.replaceAll(".", "");
             console.log(linkClass);
             var listFirst = `<li class="$classtodelete">$content[$links]</li>`; //ciascun elemento ha una sua riga, rimanda all'oggetto con href e ha content come l'argomento è chiamato
-            var listContent = '<a id="$thisclass" href="$place">$number</a> '; //dal secondo elemento si pone a fianco di quello presente
+            var listContent = '<a class="$thisclass" href="$place">$number</a> '; //dal secondo elemento si pone a fianco di quello presente
             var elements = $("#"+ what); 
             seenClasses = {}; //oggetto che contiene le classi già note
             classNames = {};
