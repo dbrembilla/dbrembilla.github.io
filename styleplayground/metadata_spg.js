951d990b898a055f6960a59f814aec0f6df8ee45
dbrembilla <davide.brembilla98@gmail.com>
2021-09-07 18:23:30 +0200

    Update metadata_spg.js

--- styleplayground/metadata_spg.js
+++ styleplayground/metadata_spg.js
@@ -182,9 +182,10 @@ String.prototype.sub = function(o) { //funzione che serve a inserire gli element
         function basefilltab(what,where) { //questo riempie le tabelle del metadata viewer
             var list = `<li class="list-quote"><a href="#$place">$content</a></li>`
             var elements = $(what); 
-            var str = elements[i].innerText
-            str = str.substr(0, 30);
+        
             for (var i=0; i<elements.length; i++) {
+                var str = elements[i].innerText
+                str = str.substr(0, 30)
                 $(where).append(list.sub({
                     place: elements[i].id,
                     content: str
