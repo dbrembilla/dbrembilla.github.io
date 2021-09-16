a05a97fde12060a84b2d2fadb2f15a89cc721acf
dbrembilla <davide.brembilla98@gmail.com>
2021-09-16 16:32:04 +0200

    Update metadata_spg.js

--- styleplayground/metadata_spg.js
+++ styleplayground/metadata_spg.js
@@ -1,23 +1,24 @@
-$('.person').mouseover(
-    $('#class-show').empty()
-    $('#class-show').append('<img src="assers/show/event.png" style="height:100%;width:auto;">')
-    console.log('pass')
+$('.person').mouseover(function()
+    {$('#class-show').empty()
+        $('#class-show').append('<img src="assers/show/event.png" style="height:100%;width:auto;">')
+        console.log('pass')}
     )
 $('.event').mouseover(
-    $('#class-show').empty()
-    $('#class-show').append('<img src="assers/show/event.png" style="height:100%;width:auto;">')
+    function(){
+        $('#class-show').empty()
+        $('#class-show').append('<img src="assers/show/event.png" style="height:100%;width:auto;">')}
     )
-$('.institution').mouseover(
+$('.institution').mouseover(function(){
     $('#class-show').empty()
-    $('#class-show').append('<img src="assers/show/institution.png" style="height:100%;width:auto;">')
+    $('#class-show').append('<img src="assers/show/institution.png" style="height:100%;width:auto;">')}
     )
-$('.quote').mouseover(
+$('.quote').mouseover(function(){
     $('#class-show').empty()
-    $('#class-show').append('<img src="assers/show/quote.png" style="height:100%;width:auto;">')
+    $('#class-show').append('<img src="assers/show/quote.png" style="height:100%;width:auto;">')}
     )
-$('.date').mouseover(
+$('.date').mouseover(function(){
     $('#class-show').empty()
-    $('#class-show').append('<img src="assers/show/date.png" style="height:100%;width:auto;">')
+    $('#class-show').append('<img src="assers/show/date.png" style="height:100%;width:auto;">')}
     )
 // Style changer
 function changeStyle(ref){
