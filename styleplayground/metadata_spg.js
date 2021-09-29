0adb12b14900f25d8da66526b8e7d51c03e7e6a3
dbrembilla <davide.brembilla98@gmail.com>
2021-09-29 21:24:24 +0200

    Update metadata_spg.js

--- styleplayground/metadata_spg.js
+++ styleplayground/metadata_spg.js
@@ -279,7 +279,7 @@ String.prototype.sub = function(o) { //funzione che serve a inserire gli element
               }));
             }
             //content: elements[i].innerHTML
-        }
+        }}
         function basefilltab(what,where) { //questo riempie le tabelle del metadata viewer
             var list = `<li class="list-quote"><a href="#$place" onclick = "highlight('quote','#$place')">$content</a></li>`;
 
