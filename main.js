ccfae95931f8701aa63aa91552e1cb7008b1bbac
dbrembilla <davide.brembilla98@gmail.com>
2022-04-12 13:33:46 +0200

    up

---
+++ main.js
@@ -0,0 +1,9 @@
+function showthis(el) {
+    displ = document.getElementById(id).style.display
+    if (displ === 'none') {
+        document.getElementById(id).style.display = show;
+    } else {
+        document.getElementById(id).style.display = none;
+    }
+    
+}
